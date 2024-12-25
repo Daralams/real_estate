c321f88cf857eb3e9994e905ed3f4da50c8d2059
daralams <strongpeople006@gmail.com>
2024-12-25 13:34:00 +0700

    [update]

--- models/estate_base_user_inherit.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-class EstateBaseUserInherit(models.Model):
-    _inherit = 'res.users'
-    _description = 'Estate Base User Inherit'
-
-    property_ids = fields.One2many('estate.property', 'salesman', string="Properties")
